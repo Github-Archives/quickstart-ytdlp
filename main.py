0b2b85e91434a34068ee548d8e21f514054a539f
Github-Archives <drewreynerwilliams@gmail.com>
2024-07-21 17:15:59 -0400

    update comment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ video_url = 'https://www.youtube.com/watch?v=7PIji8OubXU'
 # Options for yt-dlp
 ydl_opts = {
     'format': 'best',  # Download the best quality available
-    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Save to the 'downloads' folder with title as filename or reates local dir if none
+    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Save to the 'downloads' folder with title as filename or creates local dir if none exists
 }
 
 # Download the video
