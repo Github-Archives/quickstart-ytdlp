731374dfb850185b8ddfa1d3722f5d2dfc5e7f09
Github-Archives <drewreynerwilliams@gmail.com>
2024-07-21 17:15:07 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import yt_dlp
+
+# URL of the YouTube video to download
+video_url = 'https://www.youtube.com/watch?v=7PIji8OubXU'
+
+# Options for yt-dlp
+ydl_opts = {
+    'format': 'best',  # Download the best quality available
+    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Save to the 'downloads' folder with title as filename or reates local dir if none
+}
+
+# Download the video
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([video_url])
